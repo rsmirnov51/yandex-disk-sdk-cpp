a52c796dbd3ad331e2bb435641df51beda98e8f1
AlexOdlin <rockguitarist94alex@gmail.com>
2017-03-01 23:34:45 +0300

    Добавлен конструктор классов
    Конструктор классов содержит функцию передачи ключа и вывода введенного ключа

--- sources/client.cpp
+++ sources/client.cpp
@@ -1,1 +1,16 @@
+# include <iostream>
+using namespace std;
 
+class Client {
+public:
+  Client(string token);
+  {
+   token_client = token;
+   cout << "вы ввели следующий ключ: " << token_client << endl; 
+  }
+  void setClient()
+    {
+        cout << "Введите ключ: ";
+        cin >> token;
+    }
+}
