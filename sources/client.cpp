da37d54284ddbca51282215eaefe9162324ef08a
Nikita110494 <komarov.05.11.94@mail.ru>
2017-03-02 22:00:58 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -1,16 +1,37 @@
-# include <iostream>
+#include <stdio.h>
+#include <curl/curl.h>
 using namespace std;
 
 class Client {
 public:
-  Client(string token)
+
+  void UploadN()
   {
- //  token_client = token;
- //  cout << "вы ввели следующий ключ: " << token_client << endl; 
+    CURL *curl;
+  CURLcode res;
+ 
+  // Инициализация
+  curl_global_init(CURL_GLOBAL_ALL);
+ 
+  /* get a curl handle */ 
+  curl = curl_easy_init();
+  if(curl) {
+    // Установка URL, который должен получить POST 
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    //Указываем данные POST
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=Nick&project=curl");
+ 
+    //Начало выполнения запроса
+    res = curl_easy_perform(curl);
+    //Проверка на ошибки 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    //Очистка 
+    curl_easy_cleanup(curl);
   }
-  void setClient()
-    {
-   //     cout << "Введите ключ: ";
-   //     cin >> token;
-    }
+  curl_global_cleanup();
+  return 0;
+  }
 }
