7f01b5212ce1f0364cac65664d6d327d8e5f396a
bortofflex <bortofflex@ya.ru>
2017-03-02 14:00:36 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -1,7 +1,7 @@
 #include <curl/curl.h>
 
-#include <yadisk/client.hpp>
 #include <url/params.hpp>
+#include <yadisk/client.hpp>
 #include <boost/algorithm/string/join.hpp>
 
 #include <sstream>
@@ -10,7 +10,7 @@ using std::stringstream;
 #include "callbacks.hpp"
 #include "quote.hpp"
 
-namespace YandexDisk
+namespace yadisk
 {
     static const std::string api_url = "https://cloud-api.yandex.net/v1/disk/resources";
 
@@ -18,36 +18,48 @@ namespace YandexDisk
 
     auto Client::patch(url::path resource, json meta, std::list<string> fields) -> json {
 
+        // init http request
         CURL * curl = curl_easy_init();
-        url::params_t params;
-        params["fields"] = boost::algorithm::join(fields, ",");
-        params["path"] = quote(resource.string(), curl);
-        stringstream body, response;
-        std::string url = api_url + "?" + params.string();
-        auto content = meta.dump();
-        body << content;
-        std::string text = "Authorization: OAuth " + token;
 
-        curl_slist * header = nullptr;
-        header = curl_slist_append(header, "Content-Type: application/json");
-        header = curl_slist_append(header, text.c_str());
+        // fill http url
+        url::params_t url_params;
+        url_params["fields"] = boost::algorithm::join(fields, ",");
+        url_params["path"] = quote(resource.string(), curl);
+        std::string url = api_url + "?" + url_params.string();
 
+        // fill http header
+        curl_slist * http_header = nullptr;
+        std::string authorization_header = "Authorization: OAuth " + token;
+        http_header = curl_slist_append(http_header, "Content-Type: application/json");
+        http_header = curl_slist_append(http_header, authorization_header.c_str());
+
+        // fill http body
+        auto request_body = meta.dump();
+
+        // build http request
+        stringstream response_body;
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PATCH");
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_body);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write<stringstream>);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, content.c_str());
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, content.size());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request_body.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, request_body.size());
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header);
-#ifdef DEBUG
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-#endif
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, http_header);
 
-        auto result = curl_easy_perform(curl);
-        curl_slist_free_all(header);
+        // perform http request
+        auto response_code = curl_easy_perform(curl);
 
-        return result == CURLE_OK ? json::parse(response) : json();
+        // clean resources
+        curl_slist_free_all(http_header);
+        curl_easy_cleanup(curl);
+
+        // check response code
+        if ( response_code != CURLE_OK ) return json();
+
+        // handle body of http response
+        auto info = json::parse(response_body);
+        return info;
     }
 }
