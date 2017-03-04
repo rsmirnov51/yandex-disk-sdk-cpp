148f91995c190b6184a23a9cbf4686d02348a1bb
designerror <designerror@yandex.ru>
2017-03-04 11:22:56 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -17,27 +17,19 @@ namespace yadisk
     Client::Client(string token_) : token{token_} {}
 
 	auto Client::copy(url::path from, url::path to, bool overwrite, std::list<std::string> fields) -> json {
-		CURL * curl;
+
+		CURL * curl = curl_easy_init();
+		if (!curl) return json();
+		
 		url::params_t url_params;
-		struct curl_slist *header_list = nullptr;
-		std::string auth_header;
-		/*
-		 *	from=<путь к копируемому ресурсу>
-		 *	& path=<путь к скопированному ресурсу>
-		 *	[& overwrite=<признак перезаписи>]
-		 *	[& fields=<нужные ключи ответа>]
-		*/
-		
-		curl = curl_easy_init();
-		if (!curl) return "";
-		
 		url_params["from"] = quote(from.string(), curl);
 		url_params["path"] = quote(to.string(), curl);
 		url_params["overwrite"] = overwrite;
 		url_params["fields"] = boost::algorithm::join(fields, ",");
 		std::string url = api_url + "/copy?" + url_params.string();
-
-		auth_header = "Authorization: OAuth " + token;
+		
+		struct curl_slist *header_list = nullptr;
+		std::string auth_header = "Authorization: OAuth " + token;
 		header_list = curl_slist_append(header_list, auth_header.c_str());
 
 		stringstream response;
@@ -55,14 +47,15 @@ namespace yadisk
 
 		if (response_code != CURLE_OK) return json();
 
-		auto answ_data = json::parse(response);
-		return answ_data;
+		auto response_data = json::parse(response);
+		return response_data;
 	}
     
     auto Client::patch(url::path resource, json meta, std::list<string> fields) -> json {
 
         // init http request
         CURL * curl = curl_easy_init();
+	if (!curl) return json();
 
         // fill http url
         url::params_t url_params;
