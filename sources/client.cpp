8085c15152a65833c1f9ec41cd2d7a1c1819e66a
Nikita110494 <komarov.05.11.94@mail.ru>
2017-03-06 19:56:07 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -1,5 +1,5 @@
 #include <curl/curl.h>
-
+#include <stdio.h>
 #include <url/params.hpp>
 #include <yadisk/client.hpp>
 #include <boost/algorithm/string/join.hpp>
@@ -10,44 +10,33 @@ using std::stringstream;
 #include "callbacks.hpp"
 #include "quote.hpp"
 
-#include <nlohmann/json.hpp>
-using json = nlohmann::json;
+namespace yadisk
+{
+    static const std::string api_url = "https://cloud-api.yandex.net/v1/disk/resources";
 
-#include <string>
-using std::string;
-
-#include <list>
-
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
-
-#include "url/path.hpp"
-
- Client::Client(string token_) : token{token_} {}
-
-auto upload(string url, url::path from, list<string> fields) -> json
+    Client::Client(string token_) : token{token_} {}
+    auto upload(string url, url::path from, std::list<string> fields) -> json
 
   {
     	CURL *curl;
- 	 CURLcode res;
- 	url::params_t url_params;
-		struct curl_slist *header_list = nullptr;
-		std::string auth_header;
+ 	CURLcode res;
+ 	
+	struct curl_slist *header_list = nullptr;
+		
     
-    url_params["path"] = quote(to.string(), curl);
-    url_params["fields"] = boost::algorithm::join(fields, ",");
+   url::params_t url_params["path"] = quote(to.string(), curl);
+   url::params_t url_params["fields"] = boost::algorithm::join(fields, ",");
 		std::string url = api_url + "/copy?" + url_params.string();
-    auth_header = "Authorization: OAuth " + token;
+   std::string auth_header = "Authorization: OAuth " + token;
 		header_list = curl_slist_append(header_list, auth_header.c_str()); 
     stringstream response;
-  // Инициализация
-  curl_global_init(CURL_GLOBAL_ALL);
+ 
  
   curl = curl_easy_init();
    // Установка URL, который должен получить POST 
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     //Указываем данные POST
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=Nick&project=curl");
+   
       curl_easy_setopt(curl, CURLOPT_READDATA, &response);
 		curl_easy_setopt(curl, CURLOPT_READFUNCTION, write<stringstream>);
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
@@ -61,9 +50,11 @@ auto upload(string url, url::path from, list<string> fields) -> json
 
 		if (res != CURLE_OK) return json();
 
-		auto answ = json::parse(response);
+		auto answ = json::parse(res);
 		return answ;
   
+
+}
 
 };
 
