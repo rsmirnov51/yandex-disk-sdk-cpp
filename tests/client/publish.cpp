054050ecebfccccc6f09dca344ca56fdedf30f0a
rsmirnov51 <roman-smr@mail.ru>
2017-04-21 00:57:28 +0300

    Create publish.cpp

---
+++ tests/client/publish.cpp
@@ -0,0 +1,31 @@
+#include <catch.hpp>
+#include <yadisk/client.hpp>
+using ydclient = yadisk::Client;
+
+#include <string>
+#include <list>
+
+#include <url/path.hpp>
+using url::path;
+
+TEST_CASE("publish valid file with valid token", "[client][publish][file]") {
+
+	std::string token = "AQAAAAATPnx3AAQXOJS1w4zmPUdrsJNR1FATxEM";
+	path resource{ "/file.dat" };
+	ydclient client{ token };
+	auto info = client.publish(resource);
+	REQUIRE(not info.empty());
+	auto msg = info["href"].get<std::string>();
+	REQUIRE(msg == "https://cloud-api.yandex.net/v1/disk/resources?path=disk%3A%2Ffile.dat");
+}
+
+TEST_CASE("publish with invalid token", "[client][publish][file]") {
+
+	std::string token = "invalid-token";
+	path resource{ "/file.dat" };
+	ydclient client{ token };
+	auto info = client.publish(resource);
+	REQUIRE(not info.empty());
+	auto msg = info["error"].get<std::string>();
+	REQUIRE(msg == "UnauthorizedError");
+}
