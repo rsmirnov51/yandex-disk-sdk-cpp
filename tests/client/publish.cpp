fb60c2d8cdc6c27e6510bc26dd13bb38872b439a
rsmirnov51 <roman-smr@mail.ru>
2017-04-21 14:19:17 +0300

    Update publish.cpp

--- tests/client/publish.cpp
+++ tests/client/publish.cpp
@@ -29,3 +29,25 @@ TEST_CASE("publish with invalid token", "[client][publish][file]") {
 	auto msg = info["error"].get<std::string>();
 	REQUIRE(msg == "UnauthorizedError");
 }
+
+TEST_CASE("publish invalid file with valid token", "[client][publish][file]") {
+
+	std::string token = "AQAAAAATPnx3AAQXOJS1w4zmPUdrsJNR1FATxEM";
+	path resource{ "/valid-file.dat" };
+	ydclient client{ token };
+	auto info = client.publish(resource);
+	REQUIRE(not info.empty());
+	auto msg = info["error"].get<std::string>();
+	REQUIRE(msg == "DiskNotFoundError");
+}
+
+TEST_CASE("publish valid dir with valid token", "[client][publish][dir]") {
+
+	std::string token = "AQAAAAATPnx3AAQXOJS1w4zmPUdrsJNR1FATxEM";
+	path resource{ "/valid-dir/" };
+	ydclient client{ token };
+	auto info = client.publish(resource);
+	REQUIRE(not info.empty());
+	auto msg = info["href"].get<std::string>();
+	REQUIRE(msg == "https://cloud-api.yandex.net/v1/disk/resources?path=valid-dir");
+}
